3c567279c80f10b46620565252ed70342c948ada
Anusha Sreenivasan <anusha.sreeni95@gmail.com>
2017-06-19 01:53:41 +0530

    Update UserServiceImpl.java

--- marvel-universe-xmen/src/main/java/com/endurance/wolverine/xmen/service/UserServiceImpl.java
+++ marvel-universe-xmen/src/main/java/com/endurance/wolverine/xmen/service/UserServiceImpl.java
@@ -54,9 +54,5 @@ public class UserServiceImpl implements UserService{
         }
     }
 
-    @Override
-    public String getBio(String userName){
-        return userRepository.findByUserName(userName).getBio();
-    }
 
 }
